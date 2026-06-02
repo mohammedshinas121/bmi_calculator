61d1cb889acb45813bdd0557f3ae30b47fe7e9b6
mohammedshina121 <mohammedshinas121@gmail.com>
2026-06-02 15:12:07 +0530

    UI COMPLETED

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Bmi extends StatelessWidget {
+class Bmi extends StatefulWidget {
+  @override
+  State<Bmi> createState() => _BmiState();
+}
 
+class _BmiState extends State<Bmi> {
+  TextEditingController hcontroller = TextEditingController();
+
+  TextEditingController wcontroller = TextEditingController();
+
+  String result = "";
+
+  void CALCULATE() {
+    double h = double.parse(hcontroller.text) / 100;
+    double w = double.parse(wcontroller.text);
+    double bmi = w / (h * h);
+    setState(() {
+      result = "your bmi is ${bmi.toStringAsFixed(2)}";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +38,19 @@ class Bmi extends StatelessWidget {
             end: Alignment.centerLeft,
           ),
         ),
+        
         child: Center(
           child: Column(
             children: [
+              SizedBox(
+                height: 149,
+              ),
               Text(
                 "BMI CALCULATOR",
-                style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                style: GoogleFonts.abrilFatface(fontSize: 23, fontWeight: FontWeight.bold),
               ),
               SizedBox(
-                height: 500,
+                height: 350,
                 width: 400,
                 child: Card(
                   color: const Color.fromARGB(255, 255, 255, 255),
@@ -37,11 +60,12 @@ class Bmi extends StatelessWidget {
                       children: [
                         SizedBox(height: 8),
                         TextField(
+                          controller: hcontroller,
                           decoration: InputDecoration(
                             prefixIcon: Icon(Icons.height),
                             fillColor: const Color.fromARGB(255, 124, 112, 112),
                             filled: true,
-                            hintText: " HEIGHT",
+                            hintText: " HEIGHT(cm)",
                             hintStyle: TextStyle(
                               fontSize: 10,
                               fontWeight: FontWeight.bold,
@@ -53,12 +77,13 @@ class Bmi extends StatelessWidget {
                         ),
                         SizedBox(height: 8),
                         TextField(
+                          controller: wcontroller,
                           decoration: InputDecoration(
                             prefixIcon: Icon(Icons.line_weight),
                             fillColor: const Color.fromARGB(255, 124, 112, 112),
                             filled: true,
-                            hintText: "WEIGHT",
-                            hintStyle: TextStyle(
+                            hintText: "WEIGHT(kg)",
+                            hintStyle: GoogleFonts.aDLaMDisplay(
                               fontSize: 10,
                               fontWeight: FontWeight.bold,
                             ),
@@ -68,24 +93,28 @@ class Bmi extends StatelessWidget {
                           ),
                         ),
                         SizedBox(height: 13),
-                        Container(
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                const Color.fromARGB(201, 243, 207, 27),
-                                Color.fromARGB(255, 230, 230, 22),
-                              ],
+                        GestureDetector(
+                          onTap: () => CALCULATE(),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  const Color.fromARGB(201, 243, 207, 27),
+                                  Color.fromARGB(255, 230, 230, 22),
+                                ],
+                              ),
+                              borderRadius: BorderRadius.circular(15),
                             ),
-                            borderRadius: BorderRadius.circular(15)
-                          ),
-                          height: 50,
-                          width: 400,
-                          alignment: Alignment.center,
-                          child: Text(
-                            "CALCULATE",
-                            style: TextStyle(fontSize: 30),
+                            height: 50,
+                            width: 400,
+                            alignment: Alignment.center,
+                            child: Text(
+                              "CALCULATE",
+                              style: GoogleFonts.abhayaLibre(fontSize: 30),
+                            ),
                           ),
                         ),
+                        Text(result),
                       ],
                     ),
                   ),
